ea489b8df20ed21fcdf846fedb1f4a2ebf896059
ma <ma@xqijia.com>
2024-05-04 22:13:43 +0800

    fix: middlewares

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,17 +1,15 @@
 import { Context, Next } from 'koa';
 
 export async function whiteListMiddleware(ctx: Context, next: Next) {
-  try {
-    const host = ctx.request.host;
-    const whiteList = process.env.WHITELIST_DOMAINS;
-    const listStr = whiteList?.replace(/\[|\]|\"|\']/g, '');
-    const list = listStr ? listStr.split(',') : [];
-    if (!list.length) return next();
-    if (list.some(item => host.includes(item)))
-      await next();
-    else 
-      ctx.res.statusCode = 401;
-  } catch (error) {
+  const host = ctx.request.host;
+  const whiteList = process.env.WHITELIST_DOMAINS;
+  const listStr = whiteList?.replace(/\[\]/g, '');
+  const list = listStr ? listStr.split(',') : [];
+
+  if (!list.length) return next();
+  
+  if (list.some(item => host.includes(item)))
     next();
-  }
+  else 
+    ctx.res.statusCode = 401;
 }
