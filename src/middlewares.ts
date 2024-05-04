5468152d440a3aa5a8ce03b12fcf8ea3f7119aac
ma <ma@xqijia.com>
2024-05-04 22:29:23 +0800

    update: middlewares

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -3,7 +3,7 @@ import { Context, Next } from 'koa';
 export async function whiteListMiddleware(ctx: Context, next: Next) {
   const host = ctx.request.host;
   const whiteList = process.env.WHITELIST_DOMAINS;
-  const listStr = whiteList?.replace(/\[\]/g, '');
+  const listStr = whiteList?.replace(/\[\]|\"|\'/g, '');
   const list = listStr ? listStr.split(',') : [];
 
   if (!list.length) return next();
