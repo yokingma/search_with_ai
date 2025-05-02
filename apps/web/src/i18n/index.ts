43b7aabae01f4f5181d33559551c667b198b9b1c
kdeyyds <150921861+kdeyyds@users.noreply.github.com>
2025-05-02 16:53:55 +0800

    Update index.ts

--- apps/web/src/i18n/index.ts
+++ apps/web/src/i18n/index.ts
@@ -1,16 +1,16 @@
-import * as VueI18n from 'vue-i18n';
+import { createI18n } from 'vue-i18n';
 import zh from './zh';
 import en from './en';
 import ptBR from './ptBR';
 
-const i18n = VueI18n.createI18n({
-  legacy: false,
-  locale: 'en',
-  fallbackLocale: 'zh',
+const i18n = createI18n({
+  legacy: false, // 使用 Composition API 模式，更推荐
+  locale: 'en', // 默认语言
+  fallbackLocale: 'zh', // 回退语言，当当前语言没有对应翻译时使用
   messages: {
     en,
     zh,
-    ptBR
+    'pt-BR': ptBR, // 推荐使用标准的语言区域代码
   },
 });
 
