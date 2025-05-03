a1967a030788c1562dfd3444787d198a32db09d5
kdeyyds <150921861+kdeyyds@users.noreply.github.com>
2025-05-03 11:36:44 +0800

    Update index.ts
    Signed-off-by: kdeyyds <150921861+kdeyyds@users.noreply.github.com>

--- apps/web/src/components/index.ts
+++ apps/web/src/components/index.ts
@@ -1,4 +1,4 @@
-import ChatAnswer from './answer.tsx';
+import ChatAnswer from './answer.vue';
 import ChatSources from './sources.vue';
 import ChatMedia from './media.vue';
 import PageFooter from './footer.vue';
