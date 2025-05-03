59c73002e3780a222b00cd4d6846ab213356237d
kdeyyds <150921861+kdeyyds@users.noreply.github.com>
2025-05-03 10:26:11 +0800

    Update index.ts
    Signed-off-by: kdeyyds <150921861+kdeyyds@users.noreply.github.com>

--- apps/web/src/components/index.ts
+++ apps/web/src/components/index.ts
@@ -1,5 +1,18 @@
-<script setup lang="ts">
-import {
+import ChatAnswer from './answer.vue';
+import ChatSources from './sources.vue';
+import ChatMedia from './media.vue';
+import PageFooter from './footer.vue';
+import RelatedQuery from './related.vue';
+import SearchInputBar from './searchInputBar.vue';
+import ModelSelect from './models.vue';
+import SearchEngineSelect from './engine.vue';
+import LanguageSelect from './language.vue';
+import AppSettings from './settings.vue';
+import ToolBar from './toolbar.vue';
+import SearCategory from './category.vue';
+import SearchMode from './mode.vue';
+
+export {
   ChatAnswer,
   ChatSources,
   ChatMedia,
@@ -13,21 +26,4 @@ import {
   SearchMode,
   PageFooter,
   AppSettings
-} from './components';
-</script>
-
-<template>
-  <SearchInputBar />
-  <ChatAnswer />
-  <ChatSources />
-  <ChatMedia />
-  <RelatedQuery />
-  <ModelSelect />
-  <LanguageSelect />
-  <SearchEngineSelect />
-  <ToolBar />
-  <SearCategory />
-  <SearchMode />
-  <PageFooter />
-  <AppSettings />
-</template>
+};
